310b1a1e371c46dbe596dcb0bd98b605930cc0f1
Beibarysa <beibaryszhanseit@gmail.com>
2023-05-29 19:29:33 +0600

    i did 16 17 lessons

---
+++ index.js
@@ -0,0 +1,67 @@
+//1task
+
+//let age = 42
+//console.log(42)
+
+let str = "Hello world!"
+console.log(str)
+
+let isTrue = true
+console.log(isTrue)
+
+let a = 5
+let b = 3
+console.log (a+b)
+
+const pi = 3.14
+console.log(pi)
+
+//const name = "beibarys"
+//console.log(name)
+
+let age = 16
+console.log(age)
+
+let message = "Welcome to my website"
+console.log(message)
+
+let x = 10
+let y = 5
+console.log(x/y)
+
+let firstName = "beiberys"
+let secondName = "zhanseit"
+console.log("beiberys" + " zhanseit")
+
+let width = 12
+let height = 14
+console.log(width*height)
+
+let greeting = "Hello"
+const name = "Beibarys"
+console.log(greeting + " " + name)
+
+let radius = 5
+let p = 3.14
+console.log(p * radius * radius)
+
+
+let num1 = 10
+let num2 = 3
+console.log(num1 - num2)
+
+let num3 = 7
+let num4 = 2
+console.log(num3 % num4)
+
+let celcia = 10
+let farengait = (celcia * 1.8) + 32
+let kelvin = 273 + celcia
+console.log(farengait)
+console.log(kelvin)
+
+let deposit = 10000
+let rate = 0.05
+console.log(rate * deposit + deposit)
+console.log((deposit * rate + deposit) * rate + deposit)
+console.log(deposit * rate * rate * rate)
